4fbd1d5f4e8e76149da60f1716129173ef8ff085
Wesley <wesley.santos140300@gmail.com>
2024-04-13 16:48:01 -0300

    feat: create template app and component button

---
+++ lib/components/PrimaryButton.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  PrimaryButton({
+    super.key,
+    required this.title,
+    required this.extraLarge,
+    this.bgButton,
+    this.textColor,
+  });
+
+  final String title;
+  final int extraLarge;   // 1 = width 100% / 0 = % of content
+  Color? bgButton;
+  Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return extraLarge == 1 ? Column(
+      children: [
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded (
+              child: TextButton(
+                onPressed: (){},
+                style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 20, horizontal: 40)),
+                  backgroundColor: MaterialStatePropertyAll(bgButton ?? const Color.fromARGB(255, 198, 6, 6)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4), // Define o raio da borda
+                      // Pode ajustar o raio conforme necessário
+                    ),
+                  ),
+                ),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: textColor ?? Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ) : Column(
+      children: [
+        SizedBox(height: 10),
+        TextButton(
+                onPressed: (){},
+                style: ButtonStyle(
+                  padding: const MaterialStatePropertyAll(EdgeInsets.symmetric(vertical: 20, horizontal: 40)),
+                  backgroundColor: MaterialStatePropertyAll(bgButton ?? const Color.fromARGB(255, 198, 6, 6)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4), // Define o raio da borda
+                      // Pode ajustar o raio conforme necessário
+                    ),
+                  ),
+                ),
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: textColor ?? Colors.white
+                  ),
+                ),
+              ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_delivery_ponto_do_pastel/components/PrimaryButton.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,46 +8,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ponto do pastel',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 198, 6, 6)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'LOGO'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +31,181 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _currentIndex = 0;
+  String _selectedCategory  = "SALGADOS";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        backgroundColor: const Color.fromARGB(255, 231, 231, 231),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              onPressed: (){print('Clique profile navbar');}, 
+              icon: const Icon(Icons.person, color: Colors.black,),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          width: double.infinity,
+          color: const Color.fromARGB(255, 241, 241, 241),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => {
+                          setState(() {
+                            _selectedCategory = "SALGADOS";
+                          })
+                        },
+                        child: Text(
+                          'SALGADOS',
+                          style: TextStyle(
+                            color: _selectedCategory == "SALGADOS" ? Color.fromARGB(255, 198, 6, 6) : Colors.black,
+                  
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      GestureDetector(
+                        onTap: () => {
+                          setState(() {
+                            _selectedCategory = "DOCES";
+                          })
+                        },
+                        child: Text(
+                          'DOCES',
+                          style: TextStyle(
+                            color: _selectedCategory == "DOCES" ? Color.fromARGB(255, 198, 6, 6) : Colors.black,
+                  
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      GestureDetector(
+                        onTap: () => {
+                          setState(() {
+                            _selectedCategory = "BEBIDAS";
+                          })
+                        },
+                        child: Text(
+                          'BEBIDAS',
+                          style: TextStyle(
+                            color: _selectedCategory == "BEBIDAS" ? Color.fromARGB(255, 198, 6, 6) : Colors.black,
+                  
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                    ],
+                  ),
+                ),
+              ),
+            
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              color: Color.fromARGB(255, 255, 255, 255),
+              child: Column(
+                children: [
+                  PrimaryButton(title: 'Login', extraLarge: 1),
+                ],
+              ),
+            ),
+            ],
+          ),
+        ), 
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(255, 231, 231, 231),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20, right: 0, left: 20),
+          child: Column(
+            children: [
+              ListTile(
+                title: const Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Finalizar sessão',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(width: 20,),
+                    Icon(
+                      Icons.logout,
+                      color: Colors.black,
+                    )
+                  ],
+                ), 
+                onTap: (){print('Finalizar sessão');},
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 60),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      title: Text('Meu Perfil'), 
+                      onTap: (){print('Meu Perfil');},
+                    ),
+                    ListTile(
+                      title: Text('Fale Conosco'), 
+                      onTap: (){print('Fale Conosco');},
+                    ),
+                    ListTile(
+                      title: Text('Dúvidas Frequentes'), 
+                      onTap: (){print('Dúvidas Frequentes');},
+                    ),
+                  ],
+                )
+              )
+              
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => {
+          setState(() {
+            _currentIndex = index;
+          })
+        },
+        backgroundColor: Color.fromARGB(255, 251, 251, 251),
+        unselectedItemColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant_menu),
+            label: 'Cardápio',
+          ),
+          BottomNavigationBarItem(
+
+            icon: Icon(Icons.summarize),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.transparent,
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carrinho',
+          ),
+        ],
+      ),
     );
   }
 }
+
+
