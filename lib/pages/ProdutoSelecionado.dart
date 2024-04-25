4e6eae9b58636fb700a54d09f76dfdb138d34a6b
Marilia <marilialloureiro@hotmail.com>
2024-04-25 14:53:13 -0300

    feat: Criação da tela de produto selecionado

--- lib/pages/ProdutoSelecionado.dart
+++ lib/pages/ProdutoSelecionado.dart
@@ -1,5 +1,3 @@
-import 'package:app_delivery_ponto_do_pastel/components/input.dart';
-import 'package:app_delivery_ponto_do_pastel/components/primaryButton.dart';
 import 'package:flutter/material.dart';
 
 class ProdutoSelecionado extends StatefulWidget {
@@ -10,13 +8,130 @@ class ProdutoSelecionado extends StatefulWidget {
 }
 
 class _ProdutoSelecionadoState extends State<ProdutoSelecionado> {
+  final List<bool> _checkboxes = [false, false, false, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: const Text(''),
       ),
-      body: Text("Pagina produto selecionado"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            child: Column(
+              children: [
+                Image.asset('img/produtos/pastel-carne.png'),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Pastel de Carne',
+                            style: TextStyle(
+                              fontFamily: 'Outfit',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            "Massa crocante, recheada com uma carne suculenta e temperadinha, ovo e azeitonas picadas",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            "Preço: R\$",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontFamily: 'Outfit',
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "Alguma observação?",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                          const TextField(
+                            decoration: InputDecoration(
+                              labelText: "Observação",
+                              hintText:
+                                  "Caso tenha alguma observação sobre seu pedido, escreva aqui para nós =)",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "Algum adicional?",
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                          CheckboxListTile(
+                            title: const Text("Queijo mussarela - R\$3,50"),
+                            value: _checkboxes[0],
+                            onChanged: (value) {
+                              setState(() {
+                                _checkboxes[0] = value!;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                          CheckboxListTile(
+                            title: const Text("Queijo cheddar - R\$4,50"),
+                            value: _checkboxes[1],
+                            onChanged: (value) {
+                              setState(() {
+                                _checkboxes[1] = value!;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                          CheckboxListTile(
+                            title: const Text("Tomate picado - R\$2,00"),
+                            value: _checkboxes[2],
+                            onChanged: (value) {
+                              setState(() {
+                                _checkboxes[2] = value!;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                          CheckboxListTile(
+                            title: const Text("Bacon picado - R\$4,50"),
+                            value: _checkboxes[3],
+                            onChanged: (value) {
+                              setState(() {
+                                _checkboxes[3] = value!;
+                              });
+                            },
+                            controlAffinity: ListTileControlAffinity.leading,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
