edbeee35caa13e9cad82630d4f16df143de5004c
Wesley <wesley.santos140300@gmail.com>
2024-05-31 19:57:06 -0300

    feat: save

--- lib/pages/CheckoutCompra.dart
+++ lib/pages/CheckoutCompra.dart
@@ -156,7 +156,7 @@ class _FormCheckoutCompraState extends State<FormCheckoutCompra> {
     var data = {
       "carrinho_id": widget.carrinhoId,
       "usuario_id": userID,
-      "valor_total":  '0.01',
+      "valor_total":  formatToTwoDecimalPlaces(widget.valorTotalComTaxa!),
       "endereco_usuario_id": widget.enderecoUsuarioList!.isNotEmpty ? widget.enderecoUsuarioList![0]['_id'] : '',
       "lista_pagamento": [
           {
